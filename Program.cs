466c01e3820b59a9e0646d2dea818e640a92de2c
Michelle Malfabon <89042600+michellemalf@users.noreply.github.com>
2022-10-25 19:00:36 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿//Michelle Malfabon
+//Aug 16 2022
+
+//--------Mini 7----REVERSE IT-------///
+
+//Description: Takes a sequence of numbers and reverses them. 
+//Declared variables, get user input and use while loop to reverse the number.  
+
+
+
+void PlayGame()
+{
+int number;
+int remainder;
+int reverseNum = 0;
+int playAgain;
+
+//print question and store in variable (number)
+Console.WriteLine("Enter a number to reverse: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+//display user input again
+Console.WriteLine("The number is: " + number);
+
+//define function
+void ReverseNum()
+{ 
+    while (number > 0)
+    {
+        remainder = number % 10;//123 divided by 10 = 12.3; whole number is 12, R=3.
+        number= number / 10; //123/10=12
+        reverseNum= (reverseNum * 10) + remainder;
+    }
+    Console.WriteLine("The reverse is: " + reverseNum);
+}
+//calling function
+ReverseNum();
+
+Console.WriteLine("Enter 0 to play again.");
+playAgain= Convert.ToInt32(Console.ReadLine());
+if (playAgain == 0)
+{
+    PlayGame();
+}
+else{
+    Console.WriteLine("Goodbye.");
+}
+}
+PlayGame();
+
